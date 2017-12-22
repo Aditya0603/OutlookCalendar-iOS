88dd58f3467bc271de2c46d4d78f5b28cffb950b
Aditya Sinha <adityasinha@admins-MacBook-Pro-3.local>
2017-12-22 15:17:16 +0530

    Adding thread safe file write

--- OutlookCalendar/OutlookCalendar/Managers/EventManager.swift
+++ OutlookCalendar/OutlookCalendar/Managers/EventManager.swift
@@ -8,31 +8,39 @@
 
 import UIKit
 
+func synced(_ lock: Any, closure: () -> ()) {
+    objc_sync_enter(lock)
+    closure()
+    objc_sync_exit(lock)
+}
 class EventManager {
     /*
      Method to store event Models for a date
      */
     func storeEvents(eventModel : EventModel, forUniqueDate uniqueDateString : String){
         // Get documents directory path to store our events modal
-        if let docs = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
-            
-            // Append your file name to the directory path
-            // The file name here is the unique DateString
-            let path = (docs as NSString).appendingPathComponent(uniqueDateString)
-            // We need to check if a file for the uniqueDate exists or not
-            // If yes get all values and append the new one , if not then add the nerw one in new file
-            var eventModelsArr = [EventModel]()
-            if let eventModelsOldArr = self.getEventsForUniqueDateString(uniqueDateString){
-                eventModelsArr.append(contentsOf: eventModelsOldArr)
-                eventModelsArr.append(eventModel)
+        
+        // Handling thread safe for file write operation
+        synced(self, closure: {
+            if let docs = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+                // Append your file name to the directory path
+                // The file name here is the unique DateString
+                let path = (docs as NSString).appendingPathComponent(uniqueDateString)
+                // We need to check if a file for the uniqueDate exists or not
+                // If yes get all values and append the new one , if not then add the nerw one in new file
+                var eventModelsArr = [EventModel]()
+                if let eventModelsOldArr = self.getEventsForUniqueDateString(uniqueDateString){
+                    eventModelsArr.append(contentsOf: eventModelsOldArr)
+                    eventModelsArr.append(eventModel)
+                }
+                else{
+                    eventModelsArr = [EventModel]()
+                    eventModelsArr.append(eventModel)
+                }
+                // Archive your object to date file at that path
+                NSKeyedArchiver.archiveRootObject(eventModelsArr, toFile: path)
             }
-            else{
-                eventModelsArr = [EventModel]()
-                eventModelsArr.append(eventModel)
-            }
-            // Archive your object to date file at that path
-            NSKeyedArchiver.archiveRootObject(eventModelsArr, toFile: path)
-        }
+        })
     }
     
     /*
